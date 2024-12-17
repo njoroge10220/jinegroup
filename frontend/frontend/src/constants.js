34b83b23e06fd4457f5c5b12c805fc688b1b6dde
njoroge10220 <jmnjoroge13@gmail.com>
2024-12-17 09:33:16 +0300

    Restored src folder

--- frontend/frontend/src/constants.js
+++
@@ -1,6 +0,0 @@
-
-export const ACCESS_TOKEN = "access";
-export const REFRESH_TOKEN = "REFRESH";
-
-/** This will allow for use of local storage in the browser to store access and refresh tokens
-in the browser and the two exports will be the keys **/
--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Optional alias for cleaner imports
+    },
+  },
+});
